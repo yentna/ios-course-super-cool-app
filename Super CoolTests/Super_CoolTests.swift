7c56c51834b67d66bca1ccb14ec5175580bc5cd9
Kimber Gabryszak <KimberG@queenmonkey.local>
2016-02-14 22:50:54 -0700

    Initial Commit

---
+++ Super CoolTests/Super_CoolTests.swift
@@ -0,0 +1,36 @@
+//
+//  Super_CoolTests.swift
+//  Super CoolTests
+//
+//  Created by Kimber Gabryszak on 2/14/16.
+//  Copyright © 2016 Kimber. All rights reserved.
+//
+
+import XCTest
+@testable import Super_Cool
+
+class Super_CoolTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
